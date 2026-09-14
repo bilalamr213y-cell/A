809fb0b772f934fb57ca4f8bb8d520ce3af62b61
bilalamr213y-cell <bilal.amr213y@gmail.com>
2026-09-14 22:50:51 +0100

    Add files via upload

--- bot.py
+++ bot.py
@@ -22,8 +22,8 @@ INIT_URL = "https://100067.connect.garena.com/game/account_security/"
 ALGIERS_TZ = ZoneInfo("Africa/Algiers")
 START_TIME = time(4, 0, 0)  # الساعة 4 صباحاً لبدء دورة جديدة وتصفير العدادات
 INTERVAL_SECONDS = 0.001  # الفاصل الزمني بين كل إيميل
-MAX_EMAILS = 5
-MAX_DAILY_SENDS_PER_EMAIL = 10000  # الحد الأقصى للمحاولات لكل إيميل
+MAX_EMAILS = 10
+MAX_DAILY_SENDS_PER_EMAIL = 20  # الحد الأقصى للمحاولات لكل إيميل
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
